2275126f7f9de8dae9ac22026fba2e8b42669152
Alexander Liao <alexander_liao@outlook.com>
2018-02-16 23:11:58 +0000

    added training sessions

--- Deep-neural-network/Section6_1.m
+++ Deep-neural-network/Section6_1.m
@@ -7,12 +7,12 @@ for i=1:10
 
 gpuDevice(1)
 section6_1 = feedforwardnet([150 100 100 150],'trainrp');
-section6_1.trainParam.epochs = 1200;
-section6_1.trainParam.max_fail=1200;
+section6_1.trainParam.epochs = 1000;
+section6_1.trainParam.max_fail=1000;
 section6_1.output.processFcns={'fixunknowns'}
 section6_1.input.processFcns={'fixunknowns'}
 section6_1.performFcn='mse'
 
 [section6_1,tr] = train(section6_1,resM',resB','useGPU','yes');
-save(strcat('section6_1_train',num2str(i),'.mat'),'net','tr','-v7.3')
+save(strcat('section6_1_train',num2str(i),'.mat'),'section6_1','tr','-v7.3')
 end
