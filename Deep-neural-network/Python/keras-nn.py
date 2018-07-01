cce4de19cc20d30ec1011c7f923622c7d9cd22ae
Alexander Liao <alexander_liao@outlook.com>
2018-07-01 11:24:47 -0400

    update nn model

--- Deep-neural-network/Python/keras-nn.py
+++ Deep-neural-network/Python/keras-nn.py
@@ -29,7 +29,7 @@ Ytest=pd.read_csv('labelTest.csv' ,dtype='double').dropna(axis=1)
 """
 k=10000
 Xtrain=pd.read_csv('featureTrain.csv' ,dtype='double')
-Xtrain=scale(Xtrain.dropna(axis=1).loc[:, ~(Xtrain == 0).any(0)],axis=0)
+Xtrain=scale(Xtrain.dropna(axis=1).loc[:, ~(Xtrain == 0).any(0)])
 
 #Xtrain=Xtrain[0:k,:]
 
@@ -37,14 +37,14 @@ Xtrain=scale(Xtrain.dropna(axis=1).loc[:, ~(Xtrain == 0).any(0)],axis=0)
 
 #Xtrain=Xtrain.reshape((k,2,-1,1))
 
-print(Xtrain.shape)
+print(Xtrain)
 Ytrain=pd.read_csv('labelTrain.csv' ,dtype='double').dropna(axis=1)
 #Ytrain=Ytrain.values.reshape((-1,1))
 
 #Ytrain=Ytrain[0:k,:]
 
 Xtest=pd.read_csv('featureTest.csv' ,dtype='double')
-Xtest=scale(Xtest.dropna(axis=1).loc[:, ~(Xtest == 0).any(0)],axis=0)
+Xtest=scale(Xtest.dropna(axis=1).loc[:, ~(Xtest == 0).any(0)])
 
 
 #Xtest=Xtest.reshape((-1,1,len(Xtest[0,:]),1))
@@ -81,39 +81,36 @@ print(Ytrain.shape)
 #print(type(Ytrain))
 
 def nn_1(input_length):
-    #PReLU()=PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=None)
-    #BatchNormalization()=BatchNormalization()alization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None)
-    #Dropout(0.5, noise_shape=None, seed=None)=Dropout(0.5, noise_shape=None, seed=None)(0.5, noise_shape=None, seed=None)
     model = Sequential()
     model.add(Dense(32, input_dim=input_length, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
+    ##model.add(BatchNormalization())
     model.add(PReLU())
-    #model.add(Dropout(0.8))
+    #model.add(Dropout(0.25))
 
     model.add(Dense(64, input_dim=32, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
+    ##model.add(BatchNormalization())
     model.add(PReLU())
-    #model.add(Dropout(0.8))
+    #model.add(Dropout(0.25))
 
     model.add(Dense(128, input_dim=64, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
+    ##model.add(BatchNormalization())
     model.add(PReLU())
-    #model.add(Dropout(0.5))
+    #model.add(Dropout(0.25))
 
     model.add(Dense(256, input_dim=128, kernel_initializer='RandomUniform'))
     #model.add(BatchNormalization())
     model.add(PReLU())
-    #model.add(Dropout(0.5))
+    #model.add(Dropout(0.25))
 
     
     model.add(Dense(512, input_dim=256, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
+    model.add(BatchNormalization())
     model.add(PReLU())
     model.add(Dropout(0.5))
 
 
     model.add(Dense(1024, input_dim=512, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
+    model.add(BatchNormalization())
     model.add(PReLU())
     model.add(Dropout(0.5))
 
@@ -133,7 +130,7 @@ def nn_1(input_length):
     model.add(PReLU())
     model.add(Dropout(0.5))
     
-    model.add(Dense(512, input_dim=512, kernel_initializer='RandomUniform'))
+    model.add(Dense(512, input_dim=1024, kernel_initializer='RandomUniform'))
     model.add(BatchNormalization())
     model.add(PReLU())
     model.add(Dropout(0.5))
@@ -144,23 +141,23 @@ def nn_1(input_length):
     model.add(Dropout(0.5))
 
     model.add(Dense(256, input_dim=512, kernel_initializer='RandomUniform'))
-    model.add(BatchNormalization())
+    #model.add(BatchNormalization())
     model.add(PReLU())
-    model.add(Dropout(0.5))
+    #model.add(Dropout(0.5))
 
 
     model.add(Dense(128, input_dim=256, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
+    ##model.add(BatchNormalization())
     model.add(PReLU())
-    #model.add(Dropout(0.8))
+    #model.add(Dropout(0.5))
 
     model.add(Dense(64, input_dim=128, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
+    ##model.add(BatchNormalization())
     model.add(PReLU())
-    #model.add(Dropout(0.8))
+    #model.add(Dropout(0.5))
 
     model.add(Dense(32, input_dim=64, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
+    ##model.add(BatchNormalization())
     model.add(Dense(1, activation="sigmoid"))
 
     #Dense(64, input_dim=24, kernel_initializer="RandomUniform")`    
@@ -198,7 +195,7 @@ with tf.device('/gpu:0'):
         callback=keras.callbacks.TensorBoard(log_dir='./graph', histogram_freq=0, write_graph=True, write_images=True)
         print(Ytrain.shape)
         print(Xtrain.shape)
-        history=nn_predictor.fit(Xtrain,Ytrain, batch_size=1024, epochs=1500, validation_split=0.5,verbose=1, callbacks=[callback])
+        history=nn_predictor.fit(Xtrain,Ytrain, batch_size=4096, epochs=100, validation_split=0.05,verbose=1, callbacks=[callback], shuffle=True)
         
         print(type(history))
         string=routine(Ytest,nn_predictor)
