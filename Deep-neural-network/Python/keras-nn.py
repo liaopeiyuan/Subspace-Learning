a81a40cd26e495d56a7f2550817b1bc049fb9d95
Alexander Liao <alexander_liao@outlook.com>
2018-06-30 21:41:18 -0400

    added ResNet options

--- Deep-neural-network/Python/keras-nn.py
+++ Deep-neural-network/Python/keras-nn.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Dense, Dropout, BatchNormalization, PReLU, LeakyReLU
+from keras.layers import Input, Dense, Dropout, BatchNormalization, PReLU, PReLU
 from keras.models import Model, Sequential
 from keras import optimizers
 from keras import regularizers
@@ -11,9 +11,14 @@ from sklearn.preprocessing import minmax_scale
 from datetime import datetime
 import pandas as pd
 import shutil
-
+import keras
+import json
+import sys
 #a=pd.read_csv('featureTrain.csv' ,dtype='double')
 #print(a)
+
+sys.path.append('/home/alexanderliao/data/GitHub/')
+from kerasresnet import resnet
 
 """
 Xtrain=scale(pd.read_csv('featureTrain.csv' ,dtype='double').dropna(axis=1),axis=0)
@@ -22,14 +27,36 @@ Ytrain=pd.read_csv('labelTrain.csv' ,dtype='double').dropna(axis=1)
 Xtest=scale(pd.read_csv('featureTest.csv' ,dtype='double').dropna(axis=1),axis=0)
 Ytest=pd.read_csv('labelTest.csv' ,dtype='double').dropna(axis=1)
 """
-
+k=10000
 Xtrain=pd.read_csv('featureTrain.csv' ,dtype='double')
 Xtrain=scale(Xtrain.dropna(axis=1).loc[:, ~(Xtrain == 0).any(0)],axis=0)
+
+#Xtrain=Xtrain[0:k,:]
+
+#Xtrain=Xtrain.reshape((-1,1,len(Xtrain[0,:]),1))
+
+#Xtrain=Xtrain.reshape((k,2,-1,1))
+
+print(Xtrain.shape)
 Ytrain=pd.read_csv('labelTrain.csv' ,dtype='double').dropna(axis=1)
+#Ytrain=Ytrain.values.reshape((-1,1))
+
+#Ytrain=Ytrain[0:k,:]
 
 Xtest=pd.read_csv('featureTest.csv' ,dtype='double')
 Xtest=scale(Xtest.dropna(axis=1).loc[:, ~(Xtest == 0).any(0)],axis=0)
+
+
+#Xtest=Xtest.reshape((-1,1,len(Xtest[0,:]),1))
+
+
+#Xtest=Xtest.reshape((300,2,-1,1))
+
 Ytest=pd.read_csv('labelTest.csv' ,dtype='double').dropna(axis=1)
+#Ytest=Ytest.values.reshape((-1,1))
+
+#Ytest=Ytest[0:k,:]
+
 # fix random seed for reproducibility
 numpy.random.seed(7)
 #Xtrain=numpy.nonzero(numpy.loadtxt('featureTrain.csv',dtype='float32',delimiter=','))
@@ -40,7 +67,8 @@ numpy.random.seed(7)
 #Xtrain=normalize(Xtrain,axis=1)
 #Xtrain = scale( Xtrain, axis=0, with_mean=True, with_std=True, copy=True )
 #Ytrain = scale( Ytrain, axis=0, with_mean=True, with_std=True, copy=True )
-print(Xtrain)
+print(type(Xtrain))
+print(Ytrain.shape)
 #Xtest=numpy.nonzero(numpy.loadtxt('featureTest.csv',dtype='float32',delimiter=','))
 #Ytest=numpy.loadtxt('labelTest.csv',dtype='float32',delimiter=',')
 #Xtest=Xtest[1:30000,:]
@@ -53,74 +81,89 @@ print(Xtrain)
 #print(type(Ytrain))
 
 def nn_1(input_length):
-    #LeakyReLU()=PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=None)
+    #PReLU()=PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=None)
     #BatchNormalization()=BatchNormalization()alization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None)
     #Dropout(0.5, noise_shape=None, seed=None)=Dropout(0.5, noise_shape=None, seed=None)(0.5, noise_shape=None, seed=None)
     model = Sequential()
     model.add(Dense(32, input_dim=input_length, kernel_initializer='RandomUniform'))
     #model.add(BatchNormalization())
-    model.add(LeakyReLU())
-    model.add(Dropout(0.5))
+    model.add(PReLU())
+    #model.add(Dropout(0.8))
 
     model.add(Dense(64, input_dim=32, kernel_initializer='RandomUniform'))
     #model.add(BatchNormalization())
-    model.add(LeakyReLU())
-    model.add(Dropout(0.5))
+    model.add(PReLU())
+    #model.add(Dropout(0.8))
 
     model.add(Dense(128, input_dim=64, kernel_initializer='RandomUniform'))
     #model.add(BatchNormalization())
-    model.add(LeakyReLU())
-    model.add(Dropout(0.5))
+    model.add(PReLU())
+    #model.add(Dropout(0.5))
 
     model.add(Dense(256, input_dim=128, kernel_initializer='RandomUniform'))
     #model.add(BatchNormalization())
-    model.add(LeakyReLU())
-    model.add(Dropout(0.5))
+    model.add(PReLU())
+    #model.add(Dropout(0.5))
 
     
     model.add(Dense(512, input_dim=256, kernel_initializer='RandomUniform'))
     #model.add(BatchNormalization())
-    model.add(LeakyReLU())
+    model.add(PReLU())
     model.add(Dropout(0.5))
 
 
     model.add(Dense(1024, input_dim=512, kernel_initializer='RandomUniform'))
     #model.add(BatchNormalization())
-    model.add(LeakyReLU())
+    model.add(PReLU())
     model.add(Dropout(0.5))
 
-    """
+    
     model.add(Dense(2048, input_dim=1024, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
-    model.add(LeakyReLU())
+    model.add(BatchNormalization())
+    model.add(PReLU())
     model.add(Dropout(0.5))
 
     model.add(Dense(1024, input_dim=2048, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
-    model.add(LeakyReLU())
+    model.add(BatchNormalization())
+    model.add(PReLU())
     model.add(Dropout(0.5))
-    """
+    
+
+    model.add(Dense(1024, input_dim=2048, kernel_initializer='RandomUniform'))
+    #model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Dropout(0.5))
 
     model.add(Dense(512, input_dim=1024, kernel_initializer='RandomUniform'))
-    #model.add(BatchNormalization())
-    model.add(LeakyReLU())
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(512, input_dim=512, kernel_initializer='RandomUniform'))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Dropout(0.5))
+
+    model.add(Dense(512, input_dim=512, kernel_initializer='RandomUniform'))
+    model.add(BatchNormalization())
+    model.add(PReLU())
     model.add(Dropout(0.5))
 
     model.add(Dense(256, input_dim=512, kernel_initializer='RandomUniform'))
     #model.add(BatchNormalization())
-    model.add(LeakyReLU())
+    model.add(PReLU())
     model.add(Dropout(0.5))
 
 
     model.add(Dense(128, input_dim=256, kernel_initializer='RandomUniform'))
     #model.add(BatchNormalization())
-    model.add(LeakyReLU())
-    model.add(Dropout(0.5))
+    model.add(PReLU())
+    #model.add(Dropout(0.8))
 
     model.add(Dense(64, input_dim=128, kernel_initializer='RandomUniform'))
     #model.add(BatchNormalization())
-    model.add(LeakyReLU())
-    model.add(Dropout(0.5))
+    model.add(PReLU())
+    #model.add(Dropout(0.8))
 
     model.add(Dense(32, input_dim=64, kernel_initializer='RandomUniform'))
     model.add(Dense(1, activation="sigmoid"))
@@ -134,24 +177,40 @@ def nn_1(input_length):
     return model
 
 
+def routine(Ytest,nn_predictor):
+    acc=r2_score(Ytest,nn_predictor.predict(Xtest))
+    print(acc)
+    string=str(datetime.now()).replace(".","").replace(" ","")+'-'+str(round(acc,2))
+    nn_predictor.save(string+'.hdf5')
+    shutil.move(string+'.hdf5','./models/'+string+'.hdf5')
+    print(r2_score(Ytest,nn_predictor.predict(Xtest)))
+    return string
+
+
+#nn_predictor=resnet.ResnetBuilder().build_resnet_18([1,1,14],1)
+#nn_predictor=resnet.ResnetBuilder().build([1,1,14],1,'basic_block',8)
+#print(nn_predictor.input_shape)
+#print(nn_predictor.output_shape)
+
+opt = optimizers.RMSprop(lr=0.01)
+
+#nn_predictor.compile(optimizer="adadelta", loss="binary_crossentropy")
+
 nn_predictor = nn_1(len(Xtrain[0,:]))
 
 with tf.device('/gpu:0'):
     try:
-        nn_predictor.fit(Xtrain,Ytrain, batch_size=64, epochs=1000, validation_split=0.2,verbose=2)
+        callback=keras.callbacks.TensorBoard(log_dir='./graph', histogram_freq=0, write_graph=True, write_images=True)
+        print(Ytrain.shape)
+        print(Xtrain.shape)
+        history=nn_predictor.fit(Xtrain,Ytrain, batch_size=1024, epochs=1500, validation_split=0.2,verbose=1, callbacks=[callback])
+        
+        print(type(history))
+        string=routine(Ytest,nn_predictor)
+        json.dump(history.history, open( string+".json", "w" ))
+        shutil.move(string+'.json','./histories/'+string+'.pickle')
     except (KeyboardInterrupt, SystemExit):
-       
-        acc=r2_score(Ytest,nn_predictor.predict(Xtest))
-        print(acc)
-        string=str(datetime.now()).replace(".","").replace("-","").replace(":","")+'-'+round(acc,2)+'.hdf5'
-        nn_predictor.save(string)
-        shutil.move(string,'./models/'+string)
-        print(r2_score(Ytest,nn_predictor.predict(Xtest)))
+        routine(Ytest,nn_predictor)
 
-acc=r2_score(Ytest,nn_predictor.predict(Xtest))
-print(acc)
-string=str(datetime.now()).replace(".","").replace("-","").replace(":","")+'-'+round(acc,2)+'.hdf5'
-nn_predictor.save(string)
-shutil.move(string,'./models/'+string)
-print(r2_score(Ytest,nn_predictor.predict(Xtest)))
+
 
